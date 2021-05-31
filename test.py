4dc451d1e3fe1291def90c15c4a25389537ef6ee
pattons-OSU <pattons@oregonstate.edu>
2021-05-31 15:21:26 -0700

    filling in addition test on test pgm

--- test.py
+++ test.py
@@ -3,4 +3,20 @@
 """
 Simeon Patton
 CS362 OSU - Spring 2021
+Inclass Activity week 9
+GitHub actions test driver file
+
 """
+
+import unittest
+import example
+
+
+class TestCase(unittest.TestCase):
+
+    def test_add_1(self):
+        self.assertEqual(example.add(1, 2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
