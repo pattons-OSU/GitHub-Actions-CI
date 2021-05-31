523fe89fab4cf201d01f3afbe4d4f87161e67f10
pattons-OSU <pattons@oregonstate.edu>
2021-05-31 15:28:06 -0700

    implimenting functions & tests for all requir

--- example.py
+++ example.py
@@ -3,4 +3,23 @@
 """
 Simeon Patton
 CS362 OSU - Spring 2021
+Inclass Activity week 9
+GitHub actions example driver file
+
 """
+
+
+def addition(a, b):
+    return a + b
+
+
+def subtraction(a, b):
+    return a - b
+
+
+def multiplication(a, b):
+    return a * b
+
+
+def division(a, b):
+    return a / b
--- test.py
+++ test.py
@@ -14,8 +14,20 @@ import example
 
 class TestCase(unittest.TestCase):
 
-    def test_add_1(self):
+    def test_add(self):
         self.assertEqual(example.addition(1, 2), 3)
+
+
+    def test_subtract(self):
+        self.assertEqual(example.subtraction(96, 3), 93)
+
+
+    def test_multiplication(self):
+        self.assertEqual(example.multiplication(10, 2), 20)
+
+
+    def test_division(self):
+        self.assertEqual(example.division(50, 2), 25)
 
 
 if __name__ == '__main__':
